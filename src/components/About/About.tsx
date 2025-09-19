2f9d9b0f0fa3476740b2361349b23289cbc90327
신혜민 <shinhyemin225@gmail.com>
2025-09-19 23:11:20 +0900

    docs: About me 내용 수정

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,17 +4,15 @@ function About() {
       <h2 className="text-4xl mt-20 ml-4 mb-12 text-center text-[#4178a8] font-medium
 
 ">About Me</h2>
-      <p className="text-center">안녕하세요! 프론트엔드 개발자 신혜민입니다.</p>
+      <p className="text-center">안녕하세요! 신입 프론트엔드 개발자 신혜민입니다.</p>
       <p className="text-center">
-        사용자 경험을 최우선으로 생각하며, 직관적이고 뛰어난 웹
-        애플리케이션을 만드는 데 열정을 가지고 있습니다.
+      저는 사용자 경험을 가장 중요하게 생각하며, 직관적이고 사용하기 편한 웹을 만드는 걸 좋아합니다.
       </p>
       <p className="text-center">
-        새로운 기술을 배우고 적용하는 것을 즐기며, 팀과의 협업을 통해 더
-        나은 결과물을 만들어내는 것을 목표로 하고 있습니다.
+      새로운 기술을 배우고 직접 적용해보는 것도 좋아하고, 팀원들과 함께 협업하며 더 나은 결과물을 만드는 걸 목표로 하고 있어요.
       </p>
       <p className="text-center mt-4">
-        저의 포트폴리오를 통해 저의 기술과 프로젝트 경험을 확인해 주세요!
+      포트폴리오를 통해 제가 만든 프로젝트와 기술 스택을 확인해 주세요!
       </p>
     </div>
   );
