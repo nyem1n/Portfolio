434446db2790550de3a3544d9b1eca6db9114a54
신혜민 <shinhyemin225@gmail.com>
2026-06-16 15:58:52 +0900

    Tool 수정

--- src/data/cardData.tsx
+++ src/data/cardData.tsx
@@ -1,4 +1,17 @@
-import { SiReact, SiTypescript, SiTailwindcss, SiJavascript, SiHtml5, SiCss3, SiGithub, SiFigma, SiNotion, SiPostman } from "react-icons/si";
+import {
+  SiReact,
+  SiTypescript,
+  SiTailwindcss,
+  SiJavascript,
+  SiHtml5,
+  SiCss3,
+  SiGithub,
+  SiFigma,
+  SiNotion,
+  SiPostman,
+  SiMysql,
+} from "react-icons/si";
+
 import { FaTools, FaGithub } from "react-icons/fa";
 import { IoSchoolSharp } from "react-icons/io5";
 
@@ -6,44 +19,74 @@ export const cardData = {
   techStack: {
     title: (
       <span className="flex items-center gap-2">
-        <FaTools /> Tech Stacks & Tools
+        <FaTools /> Tech Stack & Tools
       </span>
     ),
+
     items: [
       { icon: <SiReact size={36} />, text: "React" },
       { icon: <SiTypescript size={36} />, text: "TypeScript" },
+
+      { icon: <SiMysql size={36} />, text: "SQL" },
+
       { icon: <SiTailwindcss size={36} />, text: "Tailwind CSS" },
+
       { icon: <SiJavascript size={36} />, text: "JavaScript (ES6+)" },
       { icon: <SiHtml5 size={36} />, text: "HTML5" },
       { icon: <SiCss3 size={36} />, text: "CSS3" },
+
       { icon: <SiGithub size={36} />, text: "Git & GitHub" },
+
       { icon: <SiFigma size={36} />, text: "Figma" },
       { icon: <SiNotion size={36} />, text: "Notion" },
       { icon: <SiPostman size={36} />, text: "Postman" },
     ],
   },
+
   education: {
     title: (
       <span className="flex items-center gap-2">
-        <IoSchoolSharp /> education
+        <IoSchoolSharp /> Education
       </span>
     ),
+
     items: [
-      { text: "동양미래대학교 정보통신공학과", description: "2021.03~2024.02 전문학사" },
-      { text: "동양미래대학교 정보통신공학과", description: "2024.03~2025.02 전공심화 학사과정" },
-      { text: "프로그래머스 데브코스 웹 풀스택 6기", description: "2025.01~2025.07" },
+      {
+        text: "동양미래대학교 정보통신공학과",
+        description: "2021.03 ~ 2024.02 전문학사",
+      },
+
+      {
+        text: "동양미래대학교 정보통신공학과",
+        description: "2024.03 ~ 2025.02 전공심화 학사과정",
+      },
+
+      {
+        text: "프로그래머스 데브코스 웹 풀스택 6기",
+        description: "2025.01 ~ 2025.07",
+      },
     ],
   },
+
   contact: {
     title: (
       <span className="flex items-center gap-2">
-        <FaGithub /> contact
+        <FaGithub /> Contact
       </span>
     ),
+
     items: [
       { text: "✉️ hmshin02@gmail.com" },
-      { text: "✍️ Velog", link: "https://velog.io/@nemin/posts" },
-      { text: "GitHub", link: "https://github.com/nyem1n" },
+
+      {
+        text: "✍️ Velog",
+        link: "https://velog.io/@nemin/posts",
+      },
+
+      {
+        text: "GitHub",
+        link: "https://github.com/nyem1n",
+      },
     ],
   },
 };
